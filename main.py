a9f13043cacaea61fb04e808f4d7e4bc5a9d617f
mayur <mayurtakwale@gmail.com>
2025-06-26 19:57:03 +0530

    modified main.py and addead data.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # flask app
+from flask import Flask
+
+app = Flask(__name__)  ### WSGI app
+
+@app.route('/') # API # Default API
+def one():
+    return "flask api"
+
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 5000, debug = True)
